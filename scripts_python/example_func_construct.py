62bbee2df137bfe9b4efccc29f29d81a3d37371d
André Luiz Caliari <alcc.biology@gmail.com>
2024-02-27 14:18:27 -0300

    generator func example fibonacci sequence

---
+++ scripts_python/example_func_construct.py
@@ -0,0 +1,42 @@
+# criando uma função geradora com yield para a sequencia de fibonacci
+# cada novo termo é a soma dos dois antecessores....
+
+import time
+
+def fib_generator(max):
+    a,b,contador=0,1,0
+    while contador<max:
+        a,b=a+b
+        yield a
+        contador+=1
+
+def fib_list(max):
+    nums=[]
+    a,b=0,1
+    while len(nums)<max:
+        nums.append(b)
+        a,b=b,a+b
+    return nums
+
+#-----------------------------------------
+# Verificando o tempo de execução da função geradora
+
+start_time_gen = time.time()
+for n in fib_generator(100000):
+    print(n)
+end_time_gen = time.time()
+
+# Verificando o tempo de execução da função gera lista e retorna da lista
+
+start_time_list = time.time()
+for n in fib_list(100000):
+    print(n)
+end_time_list = time.time()
+
+# printando o resultado do tempo de exec.
+print(f"Tempo função geradora:{end_time_gen-start_time_gen}")
+print(f"Tempo função lista:{end_time_list-start_time_list}")
+
+
+
+
