77fddcded04c07f62fd719087255e9a3d84b2d80
André Luiz Caliari <alcc.biology@gmail.com>
2024-03-04 11:08:28 -0300

    dellay in executions

--- scripts_python/aula99_package/__init__.py
+++ scripts_python/aula99_package/__init__.py
@@ -5,8 +5,8 @@
 # def dobra(x):
 #     return x*2
 # disponibilizando funções do modulo.py para outros locais
-# tudo que importarmos aqui podremos utilizar em outros locais dentro do programa,
-# o init faz a ponte de seus modulos com outros moulos
+# tudo que importarmos aqui poderemos utilizar em outros locais dentro do programa,
+# o init faz a ponte de seus modulos com outros modulos
 #from aula99_package.modulo import nova_variavel, soma_do_modulo, variavel, dobra
 # para não precisar passar todos os nomes poderemos usar o all (*), aqui,
 # é um dos poucos locais que poderemos usar este modo al
